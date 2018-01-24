55a17c4aed070149f42bbd96727907951990f716
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-01-24 16:56:54 +0530

    Create alphabetornot.c

---
+++ alphabetornot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+char s;
+printf("\nenter the character:");
+scanf("%c",&s);
+if(s=='a'||s=='b'||s=='c'||s=='d'||s=='e'||s=='f'||s=='g'||s=='h'||s=='i'||s=='j'||s=='k'||s=='l'||s=='m'||s=='n'||s=='o'||s=='p'||s=='q'||s=='r'||s=='s'||s=='t'||s=='u'||s=='v'||s=='w'||s=='x'||s=='y'||s=='z')
+printf("\n%cthe entered char is an alphabet",s);
+else 
+printf("\n%cthe entered char is not an alphabet",s);
+}
