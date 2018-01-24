ec85ee6eb1e798ac4138452815e13b09a3857a9a
Mohammedsiddiq05 <31438748+Mohammedsiddiq05@users.noreply.github.com>
2018-01-24 16:17:18 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -6,9 +6,11 @@ printf("\nenter the year:");
 scanf("%d",&n);
 if(n%400==0)
 printf("\nit is leap year%d",n);
-elseif(n%100==0)
+else if(n%100==0)
 printf("\nit is not leap year%d",n);
-elseif(n%4==0)
+else if(n%4==0)
 printf("\nit is leap year%d",n);
+ else
+   printf("\nit is not a leap year%d",n);
 return 0;
 }
